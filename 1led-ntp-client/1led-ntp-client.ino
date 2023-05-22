c82111b0f264f899808360ec29823de8afe74649
trinh7293 <trinh7293@gmail.com>
2023-05-22 20:43:42 +0700

    add functions, start sta server access

--- 1led-ntp-client/1led-ntp-client.ino
+++ 1led-ntp-client/1led-ntp-client.ino
@@ -1,13 +1,18 @@
 #include <WiFiUdp.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 #include <NTPClient.h>
 #include <FastLED.h>
 #include <TimeLib.h>
-#define NUM_LEDS 30                           // Total of 30 LED's     1X7X4+2=30. 1 pixel/segment
+#define NUM_LEDS 30                           // Total of 30 LED"s     1X7X4+2=30. 1 pixel/segment
 #define DATA_PIN D4                          // Change this if you are using another type of ESP board than a WeMos D1 Mini
 #define MILLI_AMPS 800 
 
-
+MDNSResponder mdns;
+ESP8266WebServer server(80);
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "time.nist.gov", 7*3600, 60000); //GMT+5:30 : 5*3600+30*60=19800
 
@@ -20,7 +25,17 @@ byte r_val = 255;
 byte g_val = 0;
 byte b_val = 0;
 CRGB alternateColor = CRGB::Black; 
-
+// TODO TEST add all variables for modes
+unsigned long countdownMilliSeconds;
+unsigned long endCountDownMillis;
+byte clockMode = 0; 
+byte hourFormat = 24;                         // Change this to 12 if you want default 12 hours format instead of 24               
+CRGB countdownColor = CRGB::Green;
+byte scoreboardLeft = 0;
+byte scoreboardRight = 0;
+CRGB scoreboardColorLeft = CRGB::Green;
+CRGB scoreboardColorRight = CRGB::Red;
+byte brightness = 255;
 long numbers[] = {
   0b00111111,  // [0] 0                       ///jika persegment 7 led maka setelah 0b diletakan angka 0
   0b00100001,  // [1] 1
@@ -38,10 +53,6 @@ long numbers[] = {
   0b01011100,  // [13] F(ahrenheit)
 };
 
-// set Wi-Fi SSID and password
-/* TOTO */const char *ssid     = "Phong 22";
-/* TOTO */const char *password = "@Rock1234567";
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -54,23 +65,97 @@ void setup() {
   // dht.begin();
   // pinMode(BUTTON_PIN, INPUT);
 
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting.");
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay(500);
-    Serial.print(".");
+//  WiFi.begin(ssid, password);
+//WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wm;
+  bool res;
+  // res = wm.autoConnect(); // auto generated AP name from chipid
+  // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+  res = wm.autoConnect("AutoConnectAP"); // password protected ap
+
+  if(!res) {
+      Serial.println("Failed to connect");
+      // ESP.restart();
+  } 
+  else {
+      //if you get here you have connected to the WiFi    
+      Serial.println("connected...yeey :)");
   }
-  Serial.println("connected");
+//  while ( WiFi.status() != WL_CONNECTED ) {
+//    delay(500);
+//    Serial.print(".");
+//  }
+//  Serial.println("connected");
+  Serial.println(WiFi.localIP());
+  if (mdns.begin("esp8266", WiFi.localIP()))
+  Serial.println("MDNS responder started");
+
+  server.on("/color", colorHandler);
+  server.on("/hourformat", hourformatHandler);
+  server.on("/brightness", brightnessHandler);
+  server.on("/countdown", countdownHandler);
+  server.on("/scoreboard", scoreboardHandler);
+  server.on("/clock", clockHandler);
+  // Before uploading the files with the "ESP8266 Sketch Data Upload" tool, zip the files with the command "gzip -r ./data/" (on Windows I do this with a Git Bash)
+  // *.gz files are automatically unpacked and served from your ESP (so you don't need to create a handler for each file).
+  server.serveStatic("/", SPIFFS, "/", "max-age=86400");
+  server.begin();     
+
+  SPIFFS.begin();
+  Serial.println("SPIFFS contents:");
+  Dir dir = SPIFFS.openDir("/");
+  while (dir.next()) {
+    String fileName = dir.fileName();
+    size_t fileSize = dir.fileSize();
+    Serial.printf("FS File: %s, size: %s\n", fileName.c_str(), String(fileSize).c_str());
+  }
+  Serial.println(); 
+
+  // TODO add html zip
+  
+  
   timeClient.begin();
   delay(10);
 }
 
-void updateClock() {  
-  // if (hourFormat == 12 && hour > 12)
-  //   hour = hour - 12;
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (WiFi.status() == WL_CONNECTED) { // check WiFi connection status
+    // int sensor_val = analogRead(LDR_PIN);
+    // Brightness =40;
+    server.handleClient();
+    timeClient.update();
+    // int Hours;
+    unsigned long unix_epoch = timeClient.getEpochTime();   // get UNIX Epoch time
+    Second = second(unix_epoch);                            // get seconds
+    Minute = minute(unix_epoch);                            // get minutes
+    Hour  = hour(unix_epoch);                              // get hours
+    while (millis() > time_now + period) {
+      time_now = millis();
+      // TODO TEST update all mode
+      if (clockMode == 0) {
+        updateClock();
+      } else if (clockMode == 1) {
+        updateCountdown();  
+      } else if (clockMode == 3) {
+        updateScoreboard();            
+      }
   
-  byte h1 = Hour / 10;
-  byte h2 = Hour % 10;
+      FastLED.setBrightness(brightness);
+      FastLED.show();
+    }
+  }
+}
+
+// TODO TEST add funtion to all mode
+
+void updateClock() {  
+  // TODO TEST update 12h format
+  byte hour_formated = Hour;
+  if (hourFormat == 12 && Hour > 12)
+    hour_formated = hour_formated - 12;
+  byte h1 = hour_formated / 10;
+  byte h2 = hour_formated % 10;
   byte m1 = Minute / 10;
   byte m2 = Minute % 10;  
   // byte s1 = Second / 10;
@@ -90,6 +175,104 @@ void updateClock() {
   displayDots(color);  
 }
 
+void updateCountdown() {
+
+  if (countdownMilliSeconds == 0 && endCountDownMillis == 0) 
+    return;
+    
+  unsigned long restMillis = endCountDownMillis - millis();
+  unsigned long hours   = ((restMillis / 1000) / 60) / 60;
+  unsigned long minutes = (restMillis / 1000) / 60;
+  unsigned long seconds = restMillis / 1000;
+  int remSeconds = seconds - (minutes * 60);
+  int remMinutes = minutes - (hours * 60); 
+  
+  Serial.print(restMillis);
+  Serial.print(" ");
+  Serial.print(hours);
+  Serial.print(" ");
+  Serial.print(minutes);
+  Serial.print(" ");
+  Serial.print(seconds);
+  Serial.print(" | ");
+  Serial.print(remMinutes);
+  Serial.print(" ");
+  Serial.println(remSeconds);
+
+  byte h1 = hours / 10;
+  byte h2 = hours % 10;
+  byte m1 = remMinutes / 10;
+  byte m2 = remMinutes % 10;  
+  byte s1 = remSeconds / 10;
+  byte s2 = remSeconds % 10;
+
+  CRGB color = countdownColor;
+  if (restMillis <= 60000) {
+    color = CRGB::Red;
+  }
+
+  if (hours > 0) {
+    // hh:mm
+    displayNumber(h1,3,color); 
+    displayNumber(h2,2,color);
+    displayNumber(m1,1,color);
+    displayNumber(m2,0,color);  
+  } else {
+    // mm:ss   
+    displayNumber(m1,3,color);
+    displayNumber(m2,2,color);
+    displayNumber(s1,1,color);
+    displayNumber(s2,0,color);  
+  }
+
+  displayDots(color);  
+
+  if (hours <= 0 && remMinutes <= 0 && remSeconds <= 0) {
+    Serial.println("Countdown timer ended.");
+    endCountdown();
+    countdownMilliSeconds = 0;
+    endCountDownMillis = 0;
+//    digitalWrite(COUNTDOWN_OUTPUT, HIGH);
+//    delay(500);
+//    digitalWrite(COUNTDOWN_OUTPUT,LOW);
+//    delay(500);
+//    digitalWrite(COUNTDOWN_OUTPUT, HIGH);
+//    delay(500);
+//    digitalWrite(COUNTDOWN_OUTPUT,LOW);
+//    delay(500);
+//    digitalWrite(COUNTDOWN_OUTPUT, HIGH);
+//    delay(500);
+//    digitalWrite(COUNTDOWN_OUTPUT,LOW);
+//    delay(500);
+    return;
+  }  
+}
+
+void updateScoreboard() {
+  byte sl1 = scoreboardLeft / 10;
+  byte sl2 = scoreboardLeft % 10;
+  byte sr1 = scoreboardRight / 10;
+  byte sr2 = scoreboardRight % 10;
+
+  displayNumber(sl1,3,scoreboardColorLeft);
+  displayNumber(sl2,2,scoreboardColorLeft);
+  displayNumber(sr1,1,scoreboardColorRight);
+  displayNumber(sr2,0,scoreboardColorRight);
+  hideDots();
+}
+
+void endCountdown() {
+  allBlank();
+  for (int i=0; i<NUM_LEDS; i++) {
+    if (i>0)
+      LEDs[i-1] = CRGB::Black;
+    
+    LEDs[i] = CRGB::Red;
+    FastLED.show();
+    delay(25);
+  }  
+}
+
 void displayDots(CRGB color) {
   if (dotsOn) {
     LEDs[14] = color;
@@ -100,6 +283,18 @@ void displayDots(CRGB color) {
   }
 
   dotsOn = !dotsOn;  
+}
+
+void hideDots() {
+  LEDs[14] = CRGB::Black;
+  LEDs[15] = CRGB::Black;
+}
+
+void allBlank() {
+  for (int i=0; i<NUM_LEDS; i++) {
+    LEDs[i] = CRGB::Black;
+  }
+  FastLED.show();
 }
 
 void displayNumber(byte number, byte segment, CRGB color) {
@@ -140,30 +335,47 @@ void displayNumber(byte number, byte segment, CRGB color) {
   } 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  if (WiFi.status() == WL_CONNECTED) { // check WiFi connection status
-    // int sensor_val = analogRead(LDR_PIN);
-    // Brightness =40;
-    timeClient.update();
-    // int Hours;
-    unsigned long unix_epoch = timeClient.getEpochTime();   // get UNIX Epoch time
-    Second = second(unix_epoch);                            // get seconds
-    Minute = minute(unix_epoch);                            // get minutes
-    Hour  = hour(unix_epoch);                              // get hours
-    while (millis() > time_now + period) {
-      time_now = millis();
-      updateClock();     // Show Time
-      FastLED.show();
-    }
-    // if (TIME_FORMAT == 12) {
-    //   if (Hours > 12) {
-    //     Hour = Hours - 12;
-    //   }
-    //   else
-    //     Hour = Hours;
-    // }
-    // else
-    //   Hour = Hours;
-  }
+// Handlers
+void colorHandler() {    
+  r_val = server.arg("r").toInt();
+  g_val = server.arg("g").toInt();
+  b_val = server.arg("b").toInt();
+  server.send(200, "text/json", "{\"result\":\"ok\"}");
+}
+void brightnessHandler() {    
+  brightness = server.arg("brightness").toInt();    
+  server.send(200, "text/json", "{\"result\":\"ok\"}");
+}
+
+void countdownHandler() {    
+  countdownMilliSeconds = server.arg("ms").toInt();     
+  byte cd_r_val = server.arg("r").toInt();
+  byte cd_g_val = server.arg("g").toInt();
+  byte cd_b_val = server.arg("b").toInt();
+  countdownColor = CRGB(cd_r_val, cd_g_val, cd_b_val); 
+  endCountDownMillis = millis() + countdownMilliSeconds;
+  allBlank(); 
+  clockMode = 1;     
+  server.send(200, "text/json", "{\"result\":\"ok\"}");
+}
+
+void scoreboardHandler() {   
+  scoreboardLeft = server.arg("left").toInt();
+  scoreboardRight = server.arg("right").toInt();
+  scoreboardColorLeft = CRGB(server.arg("rl").toInt(),server.arg("gl").toInt(),server.arg("bl").toInt());
+  scoreboardColorRight = CRGB(server.arg("rr").toInt(),server.arg("gr").toInt(),server.arg("br").toInt());
+  clockMode = 3;     
+  server.send(200, "text/json", "{\"result\":\"ok\"}");
+}
+
+void clockHandler() {       
+  clockMode = 0;     
+  server.send(200, "text/json", "{\"result\":\"ok\"}");
+}
+
+// TODO TEST add time format handler
+void hourformatHandler() {   
+  hourFormat = server.arg("hourformat").toInt();
+  clockMode = 0;     
+  server.send(200, "text/json", "{\"result\":\"ok\"}");
 }
